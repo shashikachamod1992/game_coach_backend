b49122ef4a22417ea971ca34ca736e3d5ebd096d
Shashika1992 <shashikachamod4u@gmail.com>
2026-08-31 23:30:07 +1200

    Init commit

---
+++ src/main/java/com/example/game_coach_backend/GameCoachBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.game_coach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCoachBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GameCoachBackendApplication.class, args);
+	}
+
+}
